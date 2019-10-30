3a1b998ad7f801f63f849a0b02eca371a59a2ab1
christophe grall <christophe.grall@gmail.com>
2019-10-30 23:49:26 +0100

    fix

--- lib/awesome_code_pipeline-stack.ts
+++ lib/awesome_code_pipeline-stack.ts
@@ -1,9 +1,55 @@
 import cdk = require('@aws-cdk/core');
+import codecommit = require("@aws-cdk/aws-codecommit");
+import codebuild = require("@aws-cdk/aws-codebuild");
+import codepipeline = require("@aws-cdk/aws-codepipeline");
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
 
 export class AwesomeCodePipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const repo = new codecommit.Repository(this, "Repository", {
+      repositoryName: "l3-dm",
+      description: "l3 app" 
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+  
+    const project = new codebuild.PipelineProject(this, 'MyProject');
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'CodeBuild',
+      project,
+      input: sourceOutput,
+      outputs: [new codepipeline.Artifact()], // optional
+    });
+
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            new codepipeline_actions.CodeCommitSourceAction({
+              actionName: 'CodeCommit_Source',
+              repository: repo,
+              output: sourceOutput,
+            }),
+          ],
+        },
+
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'Lambda_Build',
+              project,
+              input: sourceOutput,
+             
+            })
+          ]
+        }
+
+      ]});
+
   }
 }
