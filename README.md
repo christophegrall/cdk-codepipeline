8a68d71b33ff13d9a98c18f2d095511a3ab3e15c
christophe grall <christophe.grall@gmail.com>
2019-10-31 00:07:47 +0100

    update readme.md

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+ MEMO
+
+  export AWS_DEFAULT_REGION=x
+  export AWS_SECRET_ACCESS_KEY=y
+  export AWS_ACCESS_KEY_ID=z
+
+  cdk init --language typescript
+  
+  npm install @aws-cdk/aws-codebuild
+  npm install @aws-cdk/aws-codepipeline
+
+  cdk deploy --require-approval never --no-version-reporting
--- lib/awesome_code_pipeline-stack.ts
+++ lib/awesome_code_pipeline-stack.ts
@@ -3,18 +3,25 @@ import codecommit = require("@aws-cdk/aws-codecommit");
 import codebuild = require("@aws-cdk/aws-codebuild");
 import codepipeline = require("@aws-cdk/aws-codepipeline");
 import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import { Bucket } from '@aws-cdk/aws-s3';
 
 export class AwesomeCodePipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
+    let isS3present:boolean = true;
+    if(isS3present){
+      new Bucket(this, 'pp2-demo');
+    }
 
     const repo = new codecommit.Repository(this, "Repository", {
       repositoryName: "l3-dm",
       description: "l3 app" 
     });
 
-    const sourceOutput = new codepipeline.Artifact();
-  
+
+    const sourceOutput = new codepipeline.Artifact();  
     const project = new codebuild.PipelineProject(this, 'MyProject');
 
     const buildAction = new codepipeline_actions.CodeBuildAction({
